0993e6e0f719e6669132333ba17874881dd56c51
abdullah.samir <abdallahsameer22@gmail.com>
2024-07-05 01:55:07 +0300

    Maven lifecycles and Plugins

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,12 @@
 package org.example;
 
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello Apache Maven!" );
+        var name = "Abdallah";
+        System.out.println("Hello, " + name);
+     //   System.out.println(WordUtils.uncapitalize("Hello Word!!"));
     }
 }
