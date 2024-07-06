11638651b96feec2654751c5e81382e13a73f93a
abdullah.samir <abdallahsameer22@gmail.com>
2024-07-06 12:06:54 +0300

    code quality check

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,10 +2,13 @@ package org.example;
 
 import org.apache.commons.text.WordUtils;
 
+import java.util.Arrays;
+
 public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello Apache Maven!" );
         var name = "Abdallah";
         System.out.println("Hello, " + name);
@@ -13,9 +16,10 @@ public class App
     }
 
 
-
     public static void operate(int x, int y, int z, int k, int j, int l) {
         System.out.println("Here i want to check it in sonarQube");
+        int []result = new int[2];
+
         for (int i = 0; i < 100; i++);
 
         System.out.println("Hello From Java ");
