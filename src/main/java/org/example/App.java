b662009c7e0591face03a1b9296060865886cb3a
abdullah.samir <abdallahsameer22@gmail.com>
2024-07-06 21:15:57 +0300

    check central piece of information.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
+
         System.out.println( "Hello Apache Maven!" );
         var name = "Abdallah";
         System.out.println("Hello, " + name);
