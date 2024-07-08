f822a93f0fe0e99783a1162217b98e45bdcaf307
abdullah.samir <abdallahsameer22@gmail.com>
2024-07-08 10:48:47 +0300

    remove occurrences

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -44,4 +44,15 @@ public class App
         });
 
     }
+    int removeOccurrences(int[] arr, int k) {
+        int count = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == k) {
+                ++count;
+                arr[i] = arr[i] * -1;
+            }
+
+        }
+        return arr.length - count;
+    }
 }
